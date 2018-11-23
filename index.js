d4de36ba48c1d174db1f38654d1867f8119b92ae
Jeremy Apthorp <nornagon@nornagon.net>
2018-11-23 14:55:44 -0800

    add docs

--- index.js
+++ index.js
@@ -14,15 +14,52 @@ class Model {
       ...defaultModel,
       ...props,
       flds: props.flds.map((f, i) => ({...defaultField, ord: i, ...f})),
-      tmpls: props.tmpls.map((t, i) => ({...defaultTemplate, ord: i, ...t})),
+      tmpls: props.tmpls.map((t, i) => ({...defaultTemplate, ord: i, name: `Card ${i+1}`, ...t})),
     }
+    this.fieldNameToOrd = {}
+    this.props.flds.forEach(f => { this.fieldNameToOrd[f.name] = f.ord })
   }
 
   note(fields) {
-    if (fields.length !== this.props.flds.length) {
-      throw new Error(`Expected ${this.props.flds.length} fields for model '${this.props.name}' but got ${fields.length}`)
+    if (Array.isArray(fields)) {
+      if (fields.length !== this.props.flds.length) {
+        throw new Error(`Expected ${this.props.flds.length} fields for model '${this.props.name}' but got ${fields.length}`)
+      }
+      return new Note(this, fields)
+    } else {
+      const field_names = Object.keys(fields)
+      const fields_list = []
+      field_names.forEach(field_name => {
+        const ord = this.fieldNameToOrd[field_name]
+        if (ord == null) throw new Error(`Field '${field_name}' does not exist in the model`)
+        fields_list[ord] = fields[field_name]
+      })
+      return new Note(this, fields_list)
     }
-    return new Note(this, fields)
+  }
+}
+
+class ClozeModel extends Model {
+  constructor(props) {
+    super({
+      type: MODEL_CLOZE,
+      css: `
+        .card {
+          font-family: arial;
+          font-size: 20px;
+          text-align: center;
+          color: black;
+          background-color: white;
+        }
+
+        .cloze {
+          font-weight: bold;
+          color: blue;
+        }
+      `,
+      tmpls: [{name: "Cloze", ...props.tmpl}],
+      ...props
+    })
   }
 }
 
@@ -146,12 +183,6 @@ class Note {
       }
       return Array.from(ords)
     }
-  }
-}
-
-class Card {
-  constructor(ord) {
-    this.ord = ord
   }
 }
 
@@ -347,6 +378,7 @@ class Package {
 
 module.exports = {
   Model,
+  ClozeModel,
   Package,
   Deck
 }
